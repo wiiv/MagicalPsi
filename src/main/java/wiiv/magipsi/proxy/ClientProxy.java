a1c590d448e928c1f0d7c0a6129bcf8bbd1c2811
wiiv <wiivwork@gmail.com>
2017-10-25 05:18:37 +0100

    Sensor colors.

--- src/main/java/wiiv/magipsi/proxy/ClientProxy.java
+++ src/main/java/wiiv/magipsi/proxy/ClientProxy.java
@@ -1,6 +1,7 @@
 package wiiv.magipsi.proxy;
 
 import vazkii.psi.api.spell.SpellParam;
+import vazkii.psi.common.item.ItemExosuitSensor;
 import vazkii.psi.common.item.armor.ItemPsimetalArmor;
 import wiiv.magipsi.client.ModelFocusingPlate;
 
@@ -15,6 +16,12 @@ public class ClientProxy extends CommonProxy {
 		SpellParam.CYAN = 0x00FFBF;
 		SpellParam.YELLOW = 0xFFBF00; // For entities
 		SpellParam.GRAY = 0x3F3F3F; // For connectors
+		
+		ItemExosuitSensor.defaultColor = 0xEFBFFF;
+		ItemExosuitSensor.fireColor = 0xFF1F00;
+		ItemExosuitSensor.lightColor = 0xFFDF00;
+		ItemExosuitSensor.lowHealthColor = 0x7FFF00;
+		ItemExosuitSensor.underwaterColor = 0x003FFF;
 		
 		ItemPsimetalArmor.modelSupplier = ModelFocusingPlate::new;
 	}
