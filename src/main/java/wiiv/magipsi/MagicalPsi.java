dabf735d1c3b5dd18b6715325abec053546da221
wiiv <wiivwork@gmail.com>
2017-12-05 21:07:22 +0000

    Bump.

--- src/main/java/wiiv/magipsi/MagicalPsi.java
+++ src/main/java/wiiv/magipsi/MagicalPsi.java
@@ -13,7 +13,7 @@ public class MagicalPsi {
 
 	public static final String MOD_ID = "magipsi";
 	public static final String MOD_NAME = "Magical Psi";
-	public static final String VERSION = "1.2";
+	public static final String VERSION = "1.3";
 	public static final String DEPENDENCIES = "required-after:psi";
 
 	public static final String COMMON_PROXY = "wiiv.magipsi.proxy.CommonProxy";
